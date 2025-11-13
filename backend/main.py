4ad54195000fa71d5f92312adceb55531db7d845
Pablodev <pablohzdev@gmail.com>
2025-11-12 23:07:59 -0800

    fase 1 backend

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,10 @@ from typing import Any
 
 from sqlmodel import create_engine
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+DATABASE_URL = "postgresql://pabloh:@localhost/openhealth"
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 app = FastAPI(root_path="/api/v1")
 
